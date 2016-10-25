113eb254ab20ccade862bdf9aa63270851103f24
fhitchen <fhitchen@gmail.com>
2016-10-25 08:42:36 -0500

    Added script to create p4 credential.

---
+++ jenkins/init_scripts/create_p4_credentials.groovy
@@ -0,0 +1,20 @@
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import org.jenkinsci.plugins.p4.credentials.P4PasswordImpl
+import org.jenkinsci.plugins.p4.credentials.TrustImpl
+import com.cloudbees.plugins.credentials.CredentialsScope
+import com.cloudbees.plugins.credentials.domains.Domain
+
+def username = ''
+def password = ''
+
+def p4creds = new P4PasswordImpl(CredentialsScope.GLOBAL, 
+                                'p4_credentials', 
+                                'mine', 
+                                'cmildev57:1661',
+                                new TrustImpl(''),
+                                username, 
+                                '0', 
+                                '0',
+                                password)
+
+CredentialsProvider.lookupStores(Jenkins.instance).iterator().next().addCredentials(Domain.global(), p4creds)
